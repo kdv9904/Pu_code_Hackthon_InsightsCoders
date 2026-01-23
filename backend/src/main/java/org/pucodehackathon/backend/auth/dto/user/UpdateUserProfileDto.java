5227ca1f214a60ebc9f20ab42c890bfdb34c3619
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2026-01-23 22:24:54 +0530

    Main: update user profile DTO

--- backend/src/main/java/org/pucodehackathon/backend/auth/dto/user/UpdateUserProfileDto.java
+++ backend/src/main/java/org/pucodehackathon/backend/auth/dto/user/UpdateUserProfileDto.java
@@ -1,4 +1,11 @@
 package org.pucodehackathon.backend.auth.dto.user;
 
+import lombok.Data;
+
+@Data
 public class UpdateUserProfileDto {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String image;
 }
