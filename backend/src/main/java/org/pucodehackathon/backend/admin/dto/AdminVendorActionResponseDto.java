8ec74554ba36939337f24a841c899e159191afaa
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2026-01-23 19:23:42 +0530

    Email and OTP APIs are created

--- backend/src/main/java/org/pucodehackathon/backend/admin/dto/AdminVendorActionResponseDto.java
+++ backend/src/main/java/org/pucodehackathon/backend/admin/dto/AdminVendorActionResponseDto.java
@@ -9,7 +9,6 @@ import java.util.UUID;
 @Data
 @Builder
 public class AdminVendorActionResponseDto {
-
     private UUID vendorId;
     private VerificationStatus verificationStatus;
     private String message;
